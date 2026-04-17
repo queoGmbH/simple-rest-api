4b8b961d0cde76ae245ad02046515452eb1c268d
Sebastian Hofer <s.hofer@queo-group.com>
2026-04-17 10:00:02 +0200

    [TASK] Fix icon registration for TYPO3 14 compatibility
    Move icon registration to Configuration/Icons.php. TYPO3 14 no
    longer allows instantiating IconRegistry in ext_localconf.php.
    Configuration/Icons.php is supported since TYPO3 12.
    
    Related: #10

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'simple-rest-api-module' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:simple_rest_api/Resources/Public/Icons/module-simple-rest-api.svg',
+    ],
+];
